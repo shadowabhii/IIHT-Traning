1a4f0eca6dae25a5ab15f3cb9d7ca1b511f61933
shadowabhii <abhishek44221@gmail.com>
2022-08-16 18:59:34 -0700

    Initial commit

---
+++ src/main/java/com/emp/service/IEmployeeService.java
@@ -0,0 +1,13 @@
+package com.emp.service;
+
+import java.util.List;
+
+import com.emp.model.Employee;
+
+public interface IEmployeeService {
+	
+	Integer saveEmployee(Employee employee);
+	
+	public List<Employee> getAllEmployees();
+
+}
